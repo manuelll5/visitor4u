0beccae5f712415255c3f50da9c60ecdef3dff34
manuelll5 <manuel.schueler.87@gmail.com>
2019-03-12 16:40:48 +0100

    Dieverse Änderungen index.php fertig textareaAndBtnBorderInColor.css hinzugefügt (verwenbar für alle Seiten)

--- index.php
+++ index.php
@@ -3,24 +3,21 @@
   <head>
     <title>Visitor4U</title>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/textareaAndBtnBorderInColor.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
   </head>
   <body>
-    <header>
-      <?php include('navbar.php'); ?>
-    </header>
-    <!-- <div style="padding: 0px 14px">
-      <div>
-        <img src="logo/visitor4u.png" alt="V4U-Logo">
-          <div class="form-group">
-            <input type="text" name="username" autocomplete="off" placeholder="Benutzername" required>
+      <div class="center1">
+        <img src="logo/visitor4u.png" alt="V4U-Logo" class="m4u_logo">
+          <div class="form-group fg1 ">
+            <input class="form-control" type="text" name="username" autocomplete="off" placeholder="Benutzername" required>
           </div>
-          <div class="form-group">
-            <input type="password" name="password" autocomplete="off" placeholder="Passwort" required>
+          <div class="form-group fg2">
+            <input class="form-control" type="password" name="password" autocomplete="off" placeholder="Passwort" required>
           </div>
-          <input type="submit" value="Anmelden"> -->
+          <input class="btn btn-lg m4u_color" type="submit" value="Anmelden">
       </div>
-    </div>
   </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="js/navbar.js"></script>
   </head>
   <body>
