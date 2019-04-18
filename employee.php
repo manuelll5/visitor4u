cfe7975bce8b8752e81d68c146350869847e6ef8
Manuel Schüler <manuel.schueler.87@gmail.com>
2019-04-18 16:22:54 +0200

    scrollbar hinzugefügt; placeholder in color usw.

--- employee.php
+++ employee.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
+<?php // NOTE: scrollbar nur bei bedarf!!!! ?>
+<?php // NOTE: Mitarbeiter hinzufügen in center ?>
+<?php // NOTE: Table2 Rechte wie z.b reqiured ?>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/employee.css">
     <link rel="stylesheet" href="css/scrollbar.css">
+    <link rel="stylesheet" href="css/placeholder.css">
     <link rel="stylesheet" href="css/button.css">
     <link rel="stylesheet" href="css/style.css">
     <!-- <script src="js/employee.js"></script> -->
@@ -30,14 +34,14 @@
         <?php // TODO: style="display:none;" id="row" ?>
       <table class="table2 tableAll border">
         <tr>
-          <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Vorname" autocomplete="off"></td>
-          <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Nachname" autocomplete="off"></td>
-          <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
+          <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Vorname" autocomplete="off" required></td>
+          <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Nachname" autocomplete="off" required></td>
+          <td><input class="inputEmployee border" type="email" name="username" placeholder="EMail" autocomplete="off" required></td>
         </tr>
         <tr>
-          <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Abteilung" autocomplete="off"></td>
-          <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Zimmer-Nr." autocomplete="off"></td>
-          <td><input class="inputEmployee border" type="text" name="username" placeholder="Telefon-Nr." autocomplete="off"></td>
+          <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Abteilung" autocomplete="off" required></td>
+          <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Zimmer-Nr." autocomplete="off" required></td>
+          <td><input class="inputEmployee border" type="text" name="username" placeholder="Telefon-Nr." autocomplete="off" required></td>
         </tr>
       </table>
       <div style="text-align:center;">
@@ -55,48 +59,6 @@
         <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Nachname" autocomplete="off"></td>
         <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
         <td><button class="cross" type="button" value="" id="addrow"></button></td>
-      </tr>
-      <tr>
-        <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Vorname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Nachname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-      </tr>
-      <tr>
-        <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Vorname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Nachname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-      </tr>
-      <tr>
-        <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Vorname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Nachname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-      </tr>
-      <tr>
-        <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Vorname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Nachname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-      </tr>
-      <tr>
-        <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Vorname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Nachname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-      </tr>
-      <tr>
-        <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Vorname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Nachname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-      </tr>
-      <tr>
-        <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Vorname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="lastname" placeholder="Nachname" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
-        <td><input class="inputEmployee border" type="text" name="username" placeholder="EMail" autocomplete="off"></td>
       </tr>
       <tr>
         <td><input class="inputEmployee border" type="text" name="firstname" placeholder="Vorname" autocomplete="off"></td>
--- help.php
+++ help.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/help.css">
+    <link rel="stylesheet" href="css/scrollbar.css">
     <title>Visitor4U - Hilfe</title>
   </head>
   <header>
@@ -13,6 +14,7 @@
   <body>
     <?php // TODO: Responsiv! ?>
     <?php // TODO: Handbuch einfügen ?>
+    <?php // TODO: Scrollbar ersetzen ?>
     <object data="sonstiges/test.pdf" type="application/pdf">
         <?php // TODO: Test link entfernen und richtigen hinzufügen ?>
         <a href="https://www.google.de/webhp?tab=rw">Siehe Handbuch</a>
